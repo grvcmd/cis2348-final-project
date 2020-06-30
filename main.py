0dfe98d67dc34ed7c20bc4c92d669e5330da0204
Ken Valverde <verify>
2020-06-30 00:09:54 -0500

    Starting Over - created class InputFile - created instance method get_file()

--- main.py
+++ main.py
@@ -1,60 +1,35 @@
-# Ken Valverde
-# 1527936
-# CIS 2348: Final Project
-
-import sys
 import csv
+import pandas as pd
 import operator
 
-user_input = ''
 
+# User-input file class
+class InputFile:
+    # Params: name of file
+    def __init__(self, filename):
+        self.filename = filename
 
-# Get filename from user
-def get_filename():
-    global user_input
-    while user_input != 'upload':
-        try:
-            filename = input("Enter filename: ")
-            if ".csv" not in filename:
-                raise ValueError('Please include ".csv" file extension.')
+    # Define get_file function
+    def get_file(self):
 
+        # Read csv file
+        with open(self.filename, 'r') as infile:
+            reader = csv.reader(infile, delimiter=',')
 
-        except ValueError as excpt:
-            print(excpt)
-            print('Might not upload.\n')
+            # Sort self.filename alphabetically by Manufacturer (index[1] of each row)
+            sorted_list = sorted(reader, key=operator.itemgetter(1))
 
-        user_input = input("Enter any key to try again ('upload' to quit): ")
-    return filename
-
-
-def process_inventory(file):
-    try:
-        with open(file) as csvfile:
-            data = csv.reader(csvfile, delimiter=',')
-            sorted_list = sorted(data, key=operator.itemgetter(1))
-
-        with open('FullInventory.csv', 'w') as f:
-            csv_writer = csv.writer(f)
-            for row in sorted_list:
-                csv_writer.writerow(row)
-
-
-    except FileNotFoundError:
-        print("Filename: '{}' not found.".format(file))
-
-
-# OBJ 1: PROCESSED INVENTORY REPORTS:
-# OBJ 1a. Define function to write over FullInventory.csv file with inputted csv files
-
-
-# OBJ 1b. TODO: write each row in LaptopInventory.csv to their own .txt file.
-# TODO: items should be sorted by their item ID
-# TODO: Each item type (named after itself) have its own file.
-# TODO: Each row should contain the item ID, manufacturer name, price, service date, and status
+            # Write everything we read from self.filename to FullInventory.csv
+            with open('FullInventory.csv', 'w') as outfile:
+                writer = csv.writer(outfile)
+                for row in sorted_list:
+                    writer.writerow(row)
 
 
 if __name__ == '__main__':
-    print("Welcome to ManageManage\n")
 
-    file = get_filename()
-    process_inventory(file)
+    filename = input("Enter filename: ")
+    user = InputFile(filename)
+    user.get_file()
+
+
