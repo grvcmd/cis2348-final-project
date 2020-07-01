00d24960d8c3b2e5e864af4f435e73fb346cd00e
Ken Valverde <verify>
2020-06-30 19:02:11 -0500

    Starting Over

--- main.py
+++ main.py
@@ -1,35 +1,95 @@
+"""
+
+    Product inventory manager program
+    Ken Valverde
+    CIS 2348 Final Project
+
+    Program functions
+
+    - Import csv files
+    - Create Inventory reports
+    - Find item in the inventory
+
+"""
+
 import csv
-import pandas as pd
+import os
 import operator
 
 
-# User-input file class
-class InputFile:
-    # Params: name of file
-    def __init__(self, filename):
-        self.filename = filename
+# Gets filename
+def get_file():
+    file = input("Enter filename: ")
+    print("{} uploaded.\n".format(file))
+    return file + '.csv'
 
-    # Define get_file function
-    def get_file(self):
 
-        # Read csv file
-        with open(self.filename, 'r') as infile:
-            reader = csv.reader(infile, delimiter=',')
+class File:
+    def __init__(self, file):
+        self.file = file
 
-            # Sort self.filename alphabetically by Manufacturer (index[1] of each row)
+    # Outputs entire file into FullInventory.csv
+    def full_inventory(self):
+        with open(self.file, 'r') as inputfile:
+            reader = csv.reader(inputfile, delimiter=',')
+
+            # sort filename alphabetically by manufacturer (index[1])
             sorted_list = sorted(reader, key=operator.itemgetter(1))
 
-            # Write everything we read from self.filename to FullInventory.csv
-            with open('FullInventory.csv', 'w') as outfile:
+            # Write to FullInventory.csv
+            with open('FullInventory.csv', 'w', newline='') as outfile:  # newline prevents empty lines when writing
                 writer = csv.writer(outfile)
                 for row in sorted_list:
                     writer.writerow(row)
 
+    # Outputs different item types to their own files.
+    def item_type(self):
+        with open('FullInventory.csv', 'r') as inputfile:
+            reader = csv.reader(inputfile, delimiter=',')
+
+            # Iterate over each row
+            for row in reader:
+                # Iterate over column C3 only
+                for column in row[2:3]:
+
+                    # Item type: phone
+                    if column == 'phone':
+                        with open('PhoneInventory.csv', 'w', newline='') as output:
+                            writer = csv.writer(output)
+                            writer.writerow(row)
+                    # Item type: laptop
+                    elif column == 'laptop':
+                        with open('LaptopInventory.csv', 'w', newline='') as output:
+                            writer = csv.writer(output)
+                            writer.writerow(row)
+                    # Item type: tower
+                    elif column == 'tower':
+                        with open('TowerInventory.csv', 'w', newline='') as output:
+                            writer = csv.writer(output)
+                            writer.writerow(row)
+
+    def find_item(self, manufacturer, item_type):
+        try:
+            with open('FullInventory.csv', 'r') as inputfile:
+                reader = csv.reader(inputfile, delimiter=',')
+
+                # Iterate over each row
+                for row in reader:
+                    # Iterate over column C3 only
+                    for column in row[2:3]:
+                        if item_type in column:
+                            print(', '.join(row))
+        except TypeError:
+            print("\nNo such item in inventory.")
+
 
 if __name__ == '__main__':
+    user = File(get_file())
+    user.full_inventory()
+    user.item_type()
 
-    filename = input("Enter filename: ")
-    user = InputFile(filename)
-    user.get_file()
-
-
+    print("Searching for an item?")
+    manufacturer = input("Enter manufacturer: ")
+    item_type = input("Enter item type: ")
+    print('\n')
+    user.find_item(manufacturer, item_type)
